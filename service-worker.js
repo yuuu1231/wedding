7cc86040f0ded21b1b0dddde34db397d9b9d4d18
yuuu1231 <choiyujeong1231@gmail.com>
2022-07-06 18:00:19 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wedding/precache-manifest.ee5824e026a84f9b3c3ef360f694bbb1.js"
+  "/wedding/precache-manifest.3a5eda5d41c581a03139130ab7f17979.js"
 );
 
 workbox.clientsClaim();
